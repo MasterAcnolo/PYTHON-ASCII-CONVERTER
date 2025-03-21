a4cb7caca123fa232e138b6c0f65d5935ec3a1c2
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-03-21 09:53:53 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -20,8 +20,6 @@ def image_to_ascii(image_path, new_width=100):
     ascii_str = "".join(ASCII_CHARS[pixel // (255 // (len(ASCII_CHARS) - 1))] for pixel in pixels)
 
     
-    ascii_str = "\n".join(ascii_str[i:i+new_width] for i in range(0, len(ascii_str), new_width))
-
     return ascii_str
 
 
